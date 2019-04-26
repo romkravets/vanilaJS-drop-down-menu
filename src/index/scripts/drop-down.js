c717400cd761e8949c60383a581305a400b39cf9
romkravets <romann.kravets@gmail.com>
2019-04-26 17:20:16 +0300

    Added option

--- src/index/scripts/drop-down.js
+++ src/index/scripts/drop-down.js
@@ -2,9 +2,18 @@ export function dropDown(selector) {
    const menuParent = document.querySelector(selector);
    const btn = menuParent.querySelector('.drop-down-menu__btn');
    const list = menuParent.querySelector('.drop-down-menu__list');
-   console.log(btn, list);
+   const options = menuParent.querySelectorAll('.drop-down-menu__option');
+
+   for(let i = 0; i < options.length; i++) {
+      options[i].onclick = function() {
+         console.log(options[i].textContent);
+         btn.textContent = options[i].textContent;
+         list.classList.remove('drop-down-menu__list_expand');
+      }
+   }
 
    btn.onclick = function() {
       list.classList.toggle('drop-down-menu__list_expand');
+
    }
 }
