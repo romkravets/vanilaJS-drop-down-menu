87965fbea25a288a9c11c291d9048b606cf80a8c
romkravets <romann.kravets@gmail.com>
2019-04-26 16:36:28 +0300

    Added click function onclick

--- src/index/scripts/drop-down.js
+++ src/index/scripts/drop-down.js
@@ -1,3 +1,9 @@
 export function dropDown() {
-   console.log("functionDropDowm");
+   const btn = document.querySelector('.drop-down-menu__btn');
+   const list = document.querySelector('.drop-down-menu__list');
+   console.log(btn, list);
+
+   btn.onclick = function() {
+      list.classList.toggle('drop-down-menu__list_expand');
+   }
 }
