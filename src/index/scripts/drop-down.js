08a89e5d36f781377cba9ccbb29db5c3f4eecf08
romkravets <romann.kravets@gmail.com>
2019-04-26 18:27:58 +0300

    Added list function addEventListener

--- src/index/scripts/drop-down.js
+++ src/index/scripts/drop-down.js
@@ -4,26 +4,14 @@ export function dropDown(selector) {
    const list = menuParent.querySelector('.drop-down-menu__list');
    const options = menuParent.querySelectorAll('.drop-down-menu__option');
 
-   for(let i = 0; i < options.length; i++) {
-      options[i].onclick = function() {
-         console.log(options[i].textContent);
-         btn.textContent = options[i].textContent;
-         list.classList.remove('drop-down-menu__list_expand');
-      }
-   }
+   list.addEventListener('click', function(event) {
+      console.log(event.target.textContent);
+      btn.textContent = event.target.textContent;
+      list.classList.remove('drop-down-menu__list_expand');
+   })
 
    btn.addEventListener('click', function() {
       list.classList.toggle('drop-down-menu__list_expand');
 
    });
-}
-
-export function test() {
-   const btns = document.querySelectorAll('.drop-down-menu__option');
-
-   for(let i = 0; i < btns.length; i++) {
-      btns[i].addEventListener('click', function(){
-         console.log('CLICED FUNC TEST', i);
-      })
-   }
 }
